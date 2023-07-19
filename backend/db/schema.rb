a03f795d32bb17e4e4e91d415ed2674e55c5dd6c
hwng <quanghwng22@gmail.com>
2023-07-19 13:56:23 +0700

    [BE] Add association User has many Video

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_063954) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_065506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_063954) do
     t.string "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
+  add_foreign_key "videos", "users"
 end
