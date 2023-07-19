97e4a72d669568d62edeb185beb99973b19680cb
hwng <quanghwng22@gmail.com>
2023-07-19 18:29:31 +0700

    [BE] Add spec for models

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending 'add some examples to (or delete) #{__FILE__}'
+  before :each do
+    @user = User.new(email: 'foobar@eg.com', password: 'foobar')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@user).to be_valid
+  end
+
+  it 'is invalid with too short password' do
+    @user.password = '-'
+    expect(@user).to_not be_valid
+  end
+
+  it 'is invalid with too long password' do
+    @user.password = '-' * 21
+    expect(@user).to_not be_valid
+  end
+
+  it 'is invalid with uncorrect email format' do
+    @user.email = 'foobar'
+    expect(@user).to_not be_valid
+  end
+
+  it 'is invalid with duplicated email' do
+    @user.save
+    dup_user = @user.dup
+    expect(dup_user).to_not be_valid
+  end
+
+  it 'is invalid with empty email' do
+    @user.email = ''
+    expect(@user).to_not be_valid
+  end
+
+  it 'has password_digest instead' do
+    expect(@user.password_digest).to be_present
+    expect(@user.password_digest).to_not eq('foobar')
+    expect(@user.password).to eq('foobar')
+  end
 end
--- backend/spec/models/video_spec.rb
+++ backend/spec/models/video_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  pending 'add some examples to (or delete) #{__FILE__}'
+  before :each do
+    @video = Video.new(src: 'https://youtubelink.com', user: User.new)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@video).to be_valid
+  end
+
+  it 'is invalid with blank src' do
+    @video.src = ''
+    expect(@video).to_not be_valid
+  end
+
+  it 'is invalid with incorrect url format' do
+    @video.src = 'incorrect_link'
+    expect(@video).to_not be_valid
+  end
+
+  it 'is invalid without an associated user' do
+    @video.user = nil
+    expect(@video).to_not be_valid
+  end
 end
