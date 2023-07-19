ea929d364a980fd482d69ebe786ac2cd08fd69cd
hwng <quanghwng22@gmail.com>
2023-07-19 14:03:09 +0700

    [BE] Add some basic model validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :videos, dependent: :destroy
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :password, length: { in: 6...20 }
 end
--- backend/app/models/video.rb
+++ backend/app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :src, presence: true
+  validates :desc, presence: true
 end
