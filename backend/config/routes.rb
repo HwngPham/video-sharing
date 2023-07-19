e9868f2063eba606ad60b3cc7c32b429c112158f
hwng <quanghwng22@gmail.com>
2023-07-19 17:25:33 +0700

    [BE] Refactor routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :auth do
-    post 'login', to: 'login', as: 'login'
-    post 'register', to: 'register', as: 'register'
-    delete 'logout', to: 'logout', as: 'logout'
+    post 'login'
+    post 'register'
+    delete 'logout'
   end
 
-  namespace :api do
+  scope :api do
     resources :videos
   end
 end
