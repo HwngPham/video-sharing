0f9e95af2808fc20661c45164a0c616161d795c2
hwng <quanghwng22@gmail.com>
2023-07-19 18:12:30 +0700

    [BE] Add spec for routes

---
+++ backend/spec/routing/auth_routing_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe AuthController, type: :routing do
+  describe 'routing' do
+    it 'routes to #register' do
+      expect(post: 'auth/register').to route_to('auth#register')
+    end
+
+    it 'routes to #login' do
+      expect(post: 'auth/login').to route_to('auth#login')
+    end
+
+
+    it 'routes to #logout' do
+      expect(delete: 'auth/logout').to route_to('auth#logout')
+    end
+  end
+end
