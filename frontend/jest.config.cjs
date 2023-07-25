71f891e0efdebb78aaf9f460ff38d5b3c58aa34c
hwng <quanghwng22@gmail.com>
2023-07-25 16:25:02 +0700

    [FE] Mock import.meta in unittest

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -2,4 +2,24 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: "node_modules/ts-jest-mock-import-meta", // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+              options: {
+                metaObjectReplacement: { env: {} },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
 };
